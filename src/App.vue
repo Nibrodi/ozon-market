630f3d602815f7c028e634f64089458822e9cd03
Nibrodi <nibrodi@list.ru>
2020-02-01 19:02:55 +0300

    init vue-cli

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="container">
+work!
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
