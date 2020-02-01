f2db818ad35ca22e7bc8bb52db089d8d1eb82f67
Nibrodi <nibrodi@list.ru>
2020-02-01 19:25:43 +0300

    navigation panel

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app" class="container">
-work!
+    <Nav></Nav>
   </div>
 </template>
 
 <script>
+import Nav from './components/Nav'
 
 export default {
   name: 'app',
   components: {
+    Nav
   }
 }
 </script>
 
 <style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap';
+@import 'node_modules/bootstrap-vue/src/index.scss';
 
+@font-face {
+  font-family: GTEestiProDisplay;
+  src: url('./assets/fonts/GTEsti.otf');
+}
+
+.container {
+  max-width: 1024px;
+  padding: 0 44px;
+}
 </style>
